9a64a9e7d0a1d51e3dcf33e327651878e80e4379
Aleksandar Gakovic <algakovic@gmail.com>
2019-02-25 01:22:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+//- Define a function `getFirstSelector(selector)`, which accepts a selector and returns the first element that matches.
+    const comp = document.getElementById('app')
+
+  
+function getFirstSelector(selector){
+ return document.querySelector(selector)
+ 
+}
+
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+    for (let i = 0; i < lis.length; i++){
+	var msiinte = parseInt(lis[i].innerHTML) + n
+  lis[i].innerHTML = msiinte
+    }
+  }
